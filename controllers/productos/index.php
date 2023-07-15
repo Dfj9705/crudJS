5e3a86961f7404d10bce09600d8f64d67711826c
Dfj9705 <69936003+Dfj9705@users.noreply.github.com>
2023-07-14 23:01:45 -0600

    Estructura inicial

---
+++ controllers/productos/index.php
@@ -0,0 +1,18 @@
+<?php
+    header("Content-type:application/json; charset=utf-8");
+
+
+    $metodo = $_SERVER['REQUEST_METHOD'];
+
+    switch ($metodo) {
+        case 'POST':
+            # code...
+            break;
+        case 'GET':
+            # code...
+            break;
+        
+        default:
+            # code...
+            break;
+    }
